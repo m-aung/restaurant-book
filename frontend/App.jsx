d838e19668b2447e3a0183e49588eae4baeaccaf
m-aung <myothuwinaung1991@gmail.com>
2021-05-04 22:59:26 -0400

    app.jsx updated

--- frontend/App.jsx
+++ frontend/App.jsx
@@ -1,74 +1,87 @@
+// import modules
 import React from "react";
 import { Switch, Route, Link } from "react-router-dom";
 
+// import from files
 import AddReview from "./components/add-review";
 import Restaurant from "./components/restaurants";
 import RestaurantsList from "./components/restaurants-list";
 import Login from "./components/login";
 
+// functional component App
 function App() {
+  // setting initial state using react hooks
   const [user, setUser] = React.useState(null);
-
+  
+  // if user is not logged in set user to null
   async function login(user = null) {
     setUser(user);
   }
-
+  
+  // when logged out set user to null
   async function logout() {
     setUser(null)
   }
-
+  
   return (
+    // creating nav-bar
     <div>
-      <nav className="navbar navbar-expand navbar-dark bg-dark">
-        <a href="/restaurants" className="navbar-brand">
-          Restaurant Reviews
-        </a>
-        <div className="navbar-nav mr-auto">
-          <li className="nav-item">
-            <Link to={"/restaurants"} className="nav-link">
-              Restaurants
-            </Link>
-          </li>
-          <li className="nav-item" >
-            { user ? (
-              <a onClick={logout} className="nav-link" style={{cursor:'pointer'}}>
-                Logout {user.name}
-              </a>
-            ) : (            
-            <Link to={"/login"} className="nav-link">
-              Login
-            </Link>
-            )}
-
-          </li>
+    <nav className="navbar navbar-expand navbar-dark bg-dark">
+    <a href="/restaurants" className="navbar-brand">
+    Restaurant Reviews
+    </a>
+    <div className="navbar-nav mr-auto">
+    <li className="nav-item">
+    {/* restaurants route */}
+    <Link to={"/restaurants"} className="nav-link">
+    Restaurants
+    </Link>
+    </li>
+    <li className="nav-item" >
+    { user ? (
+      <a onClick={logout} className="nav-link" style={{cursor:'pointer'}}>
+      Logout {user.name}
+      </a>
+      ) : (      
+        // login route
+        <Link to={"/login"} className="nav-link">
+        Login
+        </Link>
+        )}
+        
+        </li>
         </div>
-      </nav>
-
-      <div className="container mt-3">
+        </nav>
+        
+        <div className="container mt-3">
         <Switch>
-          <Route exact path={["/", "/restaurants"]} component={RestaurantsList} />
-          <Route 
-            path="/restaurants/:id/review"
-            render={(props) => (
-              <AddReview {...props} user={user} />
-            )}
+          {/* restaurants route */}
+        <Route exact path={["/", "/restaurants"]} component={RestaurantsList} />
+        {/* Review route */}
+        <Route 
+        path="/restaurants/:id/review"
+        render={(props) => (
+          <AddReview {...props} user={user} />
+          )}
           />
+          {/* restaurants route */}
           <Route 
-            path="/restaurants/:id"
-            render={(props) => (
-              <Restaurant {...props} user={user} />
+          path="/restaurants/:id"
+          render={(props) => (
+            <Restaurant {...props} user={user} />
             )}
-          />
-          <Route 
+            />
+            {/* login route */}
+            <Route 
             path="/login"
             render={(props) => (
               <Login {...props} login={login} />
-            )}
-          />
-        </Switch>
-      </div>
-    </div>
-  );
-}
-
-export default App;
+              )}
+              />
+              </Switch>
+              </div>
+              </div>
+              );
+            }
+            
+            export default App;
