164dd194b0b4337ea4353bd85218ba689506b238
m-aung <myothuwinaung1991@gmail.com>
2021-06-03 22:56:06 -0400

    updated in login.js

--- frontend/ulti/authentication.js
+++ frontend/ulti/authentication.js
@@ -1,7 +1,7 @@
 const WEBHOOKS =
   'https://us-east-1.aws.webhooks.mongodb-realm.com/api/client/v2.0/app/restaurant_book-jpvzf/service/restaurants/incoming_webhook';
 const USERS = {};
-USERS.addUsers = async ({ username, password }) => {
+USERS.verifyUsers = async ({ username, password }) => {
   const data = {
     username,
     password,
@@ -18,15 +18,15 @@ USERS.addUsers = async ({ username, password }) => {
   };
   fetch(`${WEBHOOKS}/verify-user`, option)
     .then((res) => {
-      console.log('User Verification completed!');
-      return;
+      console.log('User Verification completed!', res);
+      return res.body;
     })
     .catch((error) => {
       console.log('from fetch: ', error);
     });
 };
 
-USERS.verifyUser = async () => {
+USERS.addUsers = async () => {
   // testing
   // return new Promise((resolve, reject) => {
   //   setTimeout(() => {
