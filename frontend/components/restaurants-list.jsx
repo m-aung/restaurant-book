cd06dddf7549fb62f6fb4274dc3b41e0e72f293a
m-aung <myothuwinaung1991@gmail.com>
2021-07-04 23:41:16 -0400

    favicon added for deployment

--- frontend/components/restaurants-list.jsx
+++ frontend/components/restaurants-list.jsx
@@ -179,7 +179,7 @@ const RestaurantsList = props => {
           const address = `${restaurant.address.building} ${restaurant.address.street}, ${restaurant.address.zipcode}`;
           return (
             <div className="col-lg-4 pb-2">
-              <div className="card ">
+              <div className="card">
                 <div className="card-body card card-modify">
                   <h5 className="card-title">{restaurant.name}</h5>
                   <p className="card-text">
